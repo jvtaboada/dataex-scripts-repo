2e814d429ebcda78e0386b040ae6fdb8bbd302b8
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-03 15:55:41 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs4E58.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs4E58.sql
@@ -1,6 +1,5 @@
 use siscoob;
 
-
 SELECT f.name AS [File Name] , f.physical_name AS [Physical Name], 
 CAST((f.size/128.0/1024) AS DECIMAL(15,2)) AS [Total Size in GB],
 CAST(f.size/128.0/1024 - CAST(FILEPROPERTY(f.name, 'SpaceUsed') AS int)/128.0/1024 AS DECIMAL(15,2)) AS [Available Space In GB],
@@ -11,4 +10,8 @@ LEFT OUTER JOIN sys.filegroups AS fg WITH (NOLOCK)
 ON f.data_space_id = fg.data_space_id
 ORDER BY f.[file_id] OPTION (RECOMPILE)
 
-DBCC SQLPERF(LOGSPACE);
+
+select top 50 DBName,FileNme,tam_gb,freespace_gb,total_gb,data from DBADataEX.dbo.DBSize 
+where DBName = 'siscoob' and FileNme = 'siscoob_log'
+order by data desc
+ 
