7b0ae38214ea5f8307a0e79403bde3ec4cf6ab07
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-03 14:55:07 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs178D.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs178D.sql
@@ -2,3 +2,21 @@ DBCC SQLPERF(LOGSPACE);
 
 use siscoob
 DBCC Opentran()
+
+select @@VERSION
+
+SELECT 
+    r.session_id,
+    r.start_time,
+    r.status,
+    r.command,
+    r.wait_type,
+    r.wait_time,
+    r.blocking_session_id,
+    DB_NAME(r.database_id) AS database_name
+FROM 
+    sys.dm_exec_requests r
+WHERE 
+    r.database_id = DB_ID('siscoob')
+ORDER BY 
+    r.start_time ASC;
