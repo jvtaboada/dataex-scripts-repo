24201a541737e5814ae6910e30531e77a87182b0
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-03 15:20:03 -0300

    Upload de arquivo automático do Power Automate

---
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs4E58.sql
@@ -0,0 +1,13 @@
+use siscoob;
+
+
+SELECT f.name AS [File Name] , f.physical_name AS [Physical Name], 
+CAST((f.size/128.0/1024) AS DECIMAL(15,2)) AS [Total Size in GB],
+CAST(f.size/128.0/1024 - CAST(FILEPROPERTY(f.name, 'SpaceUsed') AS int)/128.0/1024 AS DECIMAL(15,2)) AS [Available Space In GB],
+f.[file_id], fg.name AS [Filegroup Name],
+f.is_percent_growth, f.growth/128 as growth_MB, fg.is_default, fg.is_read_only
+FROM sys.database_files AS f WITH (NOLOCK) 
+LEFT OUTER JOIN sys.filegroups AS fg WITH (NOLOCK)
+ON f.data_space_id = fg.data_space_id
+ORDER BY f.[file_id] OPTION (RECOMPILE);
+ 
