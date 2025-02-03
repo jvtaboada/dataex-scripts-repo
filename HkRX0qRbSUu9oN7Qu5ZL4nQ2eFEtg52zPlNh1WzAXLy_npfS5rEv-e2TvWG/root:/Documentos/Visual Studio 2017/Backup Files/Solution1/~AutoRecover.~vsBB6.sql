683e4f6afcc8e1dd7bf8944221d84d4038f41a42
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-03 16:15:00 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsBB6.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsBB6.sql
@@ -1,6 +1,8 @@
 select  * 
 from [DBADataEX].[dbo].[dtx_tb_Queries_Profile] 
 with (nolock) 
-where StartTime >= GETDATE()-8 and EndTime <= GETDATE()-6
+where StartTime >= '2025-02-02 06:30:00.757'
 and databasename = 'siscoob'
-order by duration desc
+order by StartTime
+
+exec sp_Rel_Cadastro_de_Faturas_Analitico @in_empCodigo=0,@in_hotCodigo=0,@in_da=1,@in_normal=1,@in_DTCadastroIni='2025-02-01 00:00:00',@in_DTCadastroFinal='2025-02-03 00:00:00'
