7b727c4596a7cb9343c903af7f1663789d2839e3
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-03 15:25:23 -0300

    Upload de arquivo automático do Power Automate

---
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs3A1B.sql
@@ -0,0 +1,10 @@
+SELECT
+	s.name [DB_Name], 
+	f.[name] [File_Name],
+	f.physical_name,
+	--((f.size*8)/1024/1024) as [size(GB)]
+	(CAST(size as BIGINT)*8)/1024/1024 as [size(GB)]
+	FROM sys.master_files f inner join sys.databases s 
+	on f.database_id=s.database_id
+	where f.physical_name like ':%'
+	order by 4 desc
