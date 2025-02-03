0328ea28e740d3d781677435e5d1cf488d7ae41f
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-03 08:49:20 -0300

    Upload de arquivo automático do Power Automate

---
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs32D1.sql
@@ -0,0 +1,9 @@
+USE siscoob; -- Substitua pelo nome da base em análise
+GO
+
+SELECT
+name AS [Nome do Banco de Dados],
+recovery_model_desc AS [Modelo de Recuperação],
+log_reuse_wait_desc AS [Motivo de Espera de Log]
+FROM sys.databases
+WHERE name = 'siscoob';
