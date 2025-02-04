9ec47c849d9dcce26fadde583143c726e2c27715
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-04 04:51:05 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs3CF6.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs3CF6.sql
@@ -1,3 +1,5 @@
 select * from [DBADataEX].[dbo].[dtx_tb_Queries_Profile] 
 with (nolock) 
-where StartTime >= '2025-0-02 01:30:00' and EndTime <= '2023-07-12 07:30:00' order by StartTime
+where StartTime >= '2025-04-02 00:00:00' --and EndTime <= '2023-07-12 07:30:00' 
+and databasename = 'siscoob'
+order by Writes desc
