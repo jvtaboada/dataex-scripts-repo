d6bb172cfa5730bb8d76c556eb9606fa76ec2b5d
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-04 11:33:15 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs6E30.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs6E30.sql
@@ -1,2 +1,23 @@
 sp_helptext sp_Monitor_CA
 
+DECLARE @ProcedureName NVARCHAR(128) = 'sp_Monitor_CA'
+DECLARE @sql NVARCHAR(MAX)
+
+SET @sql = ''
+
+SELECT @sql = @sql + 
+'SELECT ''' + name + ''' AS DatabaseName, 
+       p.name AS ProcedureName
+ FROM ' + QUOTENAME(name) + '.sys.procedures p 
+ WHERE p.name = ''' + @ProcedureName + '''
+ UNION ALL '
+FROM sys.databases 
+WHERE state_desc = 'ONLINE'
+
+SET @sql = LEFT(@sql, LEN(@sql) - 10) -- Remove o último "UNION ALL"
+
+EXEC sp_executesql @sql
+
+
+
+sp_whoisactive
