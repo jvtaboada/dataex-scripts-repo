9017789feaaa2e06bd8f9add32a4e83629ef4997
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-04 12:43:51 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsF381.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsF381.sql
@@ -4,3 +4,6 @@ SELECT TOP (1000) [id]
       ,[dataHora]
   FROM [siscoob].[dbo].[tblHoteis_Reservas_Online_Controle_VFB_Logs]
   ORDER BY dataHora desc
+
+  SELECT TOP 5 * FROM TPtblHoteis_Reservas_Online_Controle_VFB with(nolock)
+  WHERE vfbIdentifier = @in_vfbIdentifier
