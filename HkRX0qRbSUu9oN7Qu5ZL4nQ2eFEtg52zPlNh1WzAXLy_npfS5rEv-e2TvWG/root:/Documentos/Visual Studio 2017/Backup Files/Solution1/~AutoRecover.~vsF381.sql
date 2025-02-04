f1da3f53d85de4bf362cccd59bdbcccecf0a767a
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-04 14:19:04 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsF381.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsF381.sql
@@ -1,13 +1,13 @@
-SELECT TOP (1000) [id]
-      ,[referencia]
-      ,[acao]
-      ,[dataHora]
-  FROM [siscoob].[dbo].[tblHoteis_Reservas_Online_Controle_VFB_Logs]
-  ORDER BY dataHora desc
+
 
 USE DBADataEX
 SELECT * FROM dtx_tb_Queries_Profile
 with (nolock)
-WHERE StartTime >= '2025-04-02 00:00:00'
+WHERE StartTime >= '2025-04-02 09:00:00'
 AND DataBaseName = 'siscoob'
 ORDER BY Writes DESC
+
+use siscoob
+select top 1000 *
+from sys.objects
+where type = 
