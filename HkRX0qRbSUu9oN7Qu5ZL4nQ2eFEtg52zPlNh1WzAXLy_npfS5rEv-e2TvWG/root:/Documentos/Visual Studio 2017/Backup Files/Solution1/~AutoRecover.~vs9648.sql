7bb90bd5d492f7932de4d672652acfff31b19245
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-06 16:29:36 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs9648.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs9648.sql
@@ -1,1 +1,11 @@
 sp_whoisactive
+
+select top 1000 *
+from DBADataEX.dbo.dtx_tb_Queries_Profile
+--where TextData like 'update jdt1 set profitcode%'
+order by StartTime desc
+
+--Bloqueio 15h30 - 15h50
+
+
+--Bloqueio 15h50 - 16h
