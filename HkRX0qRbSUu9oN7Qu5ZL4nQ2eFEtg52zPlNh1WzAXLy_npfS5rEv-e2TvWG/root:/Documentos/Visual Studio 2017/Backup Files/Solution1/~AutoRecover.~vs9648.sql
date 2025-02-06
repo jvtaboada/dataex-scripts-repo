e23d25014283268c5f344ab1bd2e69bddc472348
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-06 16:34:55 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs9648.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs9648.sql
@@ -2,10 +2,14 @@ sp_whoisactive
 
 select top 1000 *
 from DBADataEX.dbo.dtx_tb_Queries_Profile
---where TextData like 'update jdt1 set profitcode%'
+--where TextData like 'update%'
+where HostName = 'ASCSRV-TS-SAP01'
 order by StartTime desc
 
 --Bloqueio 15h30 - 15h50
-
+hostname fica como o ts: ASCSRV-TS-SAP01
+consulta consegui: update jdt1 set profitcode = @Centro where BaseRef =@list_of_cols_val_tab_del and ShortName (...)
 
 --Bloqueio 15h50 - 16h
+hostanem também fica como o TS: ASCSRV-TS-SAP01
+consulta fica como: update jdt1 set OcrCode2 = @Unidadenegocios where BaseRef =@list_of_cols_val_tab_del and ShortN
