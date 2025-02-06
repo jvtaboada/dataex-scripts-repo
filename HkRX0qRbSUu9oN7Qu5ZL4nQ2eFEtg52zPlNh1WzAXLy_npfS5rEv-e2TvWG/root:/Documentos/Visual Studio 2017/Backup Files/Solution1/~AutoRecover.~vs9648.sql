bd716654ee422c64a4fb644400986bb8c76105b4
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-06 16:49:32 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs9648.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs9648.sql
@@ -11,6 +11,12 @@ hostname fica como o ts: ASCSRV-TS-SAP01
 consulta consegui: update jdt1 set profitcode = @Centro where BaseRef =@list_of_cols_val_tab_del and ShortName (...)
 EXECUTE SBO_SP_TransactionNotification N'13',N'U',1,N'DocEntry',N'159790'
 
+@object_type nvarchar(20) = 13,        -- SBO Object Type
+@transaction_type nchar(1) = UPDATE,     -- [A]dd, [U]pdate, [D]elete, [C]ancel, C[L]ose
+@num_of_cols_in_key int = 1,
+@list_of_key_cols_tab_del = DocEntry nvarchar(255),
+@list_of_cols_val_tab_del = 159790 nvarchar(255)
+
 --Bloqueio 15h50 - 16h
 hostanem também fica como o TS: ASCSRV-TS-SAP01
 consulta fica como: update jdt1 set OcrCode2 = @Unidadenegocios where BaseRef =@list_of_cols_val_tab_del and ShortN
