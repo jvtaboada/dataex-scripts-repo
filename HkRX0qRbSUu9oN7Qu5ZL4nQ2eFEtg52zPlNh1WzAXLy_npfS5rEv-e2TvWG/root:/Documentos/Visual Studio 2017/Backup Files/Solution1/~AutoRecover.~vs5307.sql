9bdf3f05bd686ae88d6c07cee3a1ea3d59fcda1a
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-14 14:40:57 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs5307.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs5307.sql
@@ -2,12 +2,12 @@ sp_whoisactive
 
 use siscoob;
 
-SELECT  
+SELECT TOP 10  
     s.[name] AS SchemaName,  
     t.[name] AS TableName,  
     i.[name] AS IndexName,  
     i.[type_desc] AS IndexType,  
-    SUM(a.total_pages) * 8 AS IndexSizeKB  
+    CAST(SUM(a.total_pages) * 8 / 1024.0 AS DECIMAL(18,2)) AS IndexSizeMB  
 FROM sys.indexes i  
 JOIN sys.tables t ON i.[object_id] = t.[object_id]  
 JOIN sys.schemas s ON t.[schema_id] = s.[schema_id]  
@@ -15,4 +15,4 @@ JOIN sys.partitions p ON i.[object_id] = p.[object_id] AND i.[index_id] = p.[ind
 JOIN sys.allocation_units a ON p.[partition_id] = a.[container_id]  
 WHERE i.[type] IN (1, 2)  -- Clustered e Non-clustered indexes  
 GROUP BY s.[name], t.[name], i.[name], i.[type_desc]  
-ORDER BY IndexSizeKB DESC;
+ORDER BY IndexSizeMB DESC;
