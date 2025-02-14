2478f6d2e0c8cf766995c1dfbe293d9a4caec5c0
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-14 11:55:42 -0300

    Upload de arquivo automático do Power Automate

---
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsC9CB.sql
@@ -0,0 +1,15 @@
+sp_whoisactive
+
+use siscoob
+
+SELECT 
+    dbschemas.[name] AS SchemaName, 
+    dbtables.[name] AS TableName, 
+    dbindexes.[name] AS IndexName, 
+    indexstats.avg_fragmentation_in_percent 
+FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'LIMITED') AS indexstats
+JOIN sys.tables dbtables ON dbtables.[object_id] = indexstats.[object_id]
+JOIN sys.schemas dbschemas ON dbtables.[schema_id] = dbschemas.[schema_id]
+JOIN sys.indexes AS dbindexes ON dbindexes.[object_id] = indexstats.[object_id] AND indexstats.index_id = dbindexes.index_id
+WHERE indexstats.avg_fragmentation_in_percent > 30
+ORDER BY indexstats.avg_fragmentation_in_percent DESC;
