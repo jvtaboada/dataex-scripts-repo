33a63253b910b3f067d55d665b432b713abb6391
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-14 12:30:58 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs7590.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs7590.sql
@@ -1,1 +1,20 @@
 sp_whoisactive
+
+use DBADataEX
+go
+declare @dia_ant as char(8)
+select @dia_ant = convert(varchar, getdate()-1, 112)	
+select B.name,
+A.err_timestamp,
+a.err_severity,
+a.err_number,
+a.username,
+a.database_id,
+replace(replace(a.err_message, char(13),''), char(10), '') as err_message,
+replace(replace(a.sql_text, char(13),''), char(10), '') as sql_text,
+a.client_hostname,
+Dt_Error
+from dbo.[dtx_tb_Log_DB_Error] A (nolock)
+join sys.databases B (nolock) on A.database_id = B.database_id
+where err_timestamp >= @dia_ant --and err_timestamp < '20210922'
+order by err_timestamp
