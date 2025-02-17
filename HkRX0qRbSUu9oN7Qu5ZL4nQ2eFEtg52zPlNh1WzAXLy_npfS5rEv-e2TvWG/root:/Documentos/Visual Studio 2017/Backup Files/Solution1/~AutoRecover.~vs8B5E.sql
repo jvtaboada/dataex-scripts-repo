b750c8c2ec13a40e9568292b1108c0bf9ff98b0c
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-17 11:02:13 -0300

    Upload de arquivo automático do Power Automate

---
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs8B5E.sql
@@ -0,0 +1,17 @@
+USE SISCOOB
+
+SELECT 
+    DB_NAME(ps.database_id) AS DatabaseName,
+    OBJECT_NAME(ps.object_id, ps.database_id) AS TableName,
+    i.name AS IndexName,
+    ps.index_id,
+    ps.index_type_desc,
+    ps.avg_fragmentation_in_percent,
+    ps.page_count
+FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'LIMITED') AS ps
+JOIN sys.indexes AS i 
+    ON ps.object_id = i.object_id 
+    AND ps.index_id = i.index_id
+WHERE ps.index_id > 0  -- Exclui heaps (tabelas sem índice clustered)
+AND ps.avg_fragmentation_in_percent > 30
+ORDER BY ps.avg_fragmentation_in_percent DESC;
