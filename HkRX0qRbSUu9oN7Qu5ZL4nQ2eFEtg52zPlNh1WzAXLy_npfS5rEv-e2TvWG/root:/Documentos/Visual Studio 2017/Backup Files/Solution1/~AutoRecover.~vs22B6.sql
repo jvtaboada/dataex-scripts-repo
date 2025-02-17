c989048032a1e71df9e49500b7b8ddfd021e9e4d
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-02-17 11:12:34 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs22B6.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs22B6.sql
@@ -1,1 +1,9 @@
 sp_whoisactive
+
+SELECT 
+    DB_NAME(database_id) AS DatabaseName,
+    SUM(size * 8) / 1024 AS SizeMB -- Convertendo de páginas (8 KB) para MB
+FROM sys.master_files
+WHERE type = 0 -- Apenas arquivos de dados (exclui logs)
+GROUP BY database_id
+ORDER BY SizeMB DESC;
