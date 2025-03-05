4a53cb951f2da506dfee27caca10d6116b298c8d
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-03-05 11:47:43 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsB9B6.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsB9B6.sql
@@ -3,6 +3,28 @@ FROM [DBADataex].[dbo].[dtx_tb_Alert_Parameter]
 WHERE Nm_Alert LIKE '%Restarted%'
 
 INSERT INTO [DBADataex].[dbo].[dtx_tb_Alert_Parameter]
-(Nm_Alert, )
+			([Nm_Alert]
+           ,[Nm_Procedure]
+           ,[Fl_Language]
+           ,[Fl_Clear]
+           ,[Fl_Enable]
+           ,[Vl_Parameter]
+           ,[Ds_Metric]
+           ,[Vl_Parameter_2]
+           ,[Ds_Metric_2]
+           ,[Ds_Profile_Email]
+           ,[Ds_Email]
+           ,[Ds_Message_Alert_ENG]
+           ,[Ds_Message_Clear_ENG]
+           ,[Ds_Message_Alert_PTB]
+           ,[Ds_Message_Clear_PTB]
+           ,[Ds_Email_Information_1_ENG]
+           ,[Ds_Email_Information_2_ENG]
+           ,[Ds_Email_Information_1_PTB]
+           ,[Ds_Email_Information_2_PTB])
 VALUES 
-()
+			('SQL Server Restarted PagerDuty',
+			'dtx_sp_Alert_SQLServer_Restarted_PagerDuty',
+			1', 
+			0
+			)
