1d8c35c27150878a5e669c8e87a0b657f715b840
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-04-08 09:15:26 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsE80F.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsE80F.sql
@@ -3,3 +3,15 @@ SELECT
     DATEDIFF(HOUR, sqlserver_start_time, SYSDATETIME()) AS [Uptime in Hours],
     DATEDIFF(MINUTE, sqlserver_start_time, SYSDATETIME()) AS [Uptime in Minutes]
 FROM sys.dm_os_sys_info;
+
+SELECT 
+    name AS [Database Name],
+    state_desc AS [State],
+    recovery_model_desc AS [Recovery Model],
+    user_access_desc AS [User Access],
+    is_read_only AS [Read-Only],
+    is_auto_close_on AS [Auto Close],
+    is_auto_shrink_on AS [Auto Shrink],
+    create_date AS [Created On]
+FROM sys.databases
+ORDER BY name;
