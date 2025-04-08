369097bc0d919b1935edf0272661386187115b39
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-04-08 16:37:04 -0300

    Upload de arquivo automático do Power Automate

---
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vs2D40.sql
@@ -0,0 +1,45 @@
+USE [DBADataex]
+GO
+/****** Object:  Table [dbo].[dtx_tb_Rel_Database_Apoio]    Script Date: 4/8/2025 3:47:34 PM ******/
+DROP TABLE [dbo].[dtx_tb_Rel_Database_Apoio]
+GO
+USE [DBADataex]
+GO
+/****** Object:  Table [dbo].[dtx_tb_Rel_Database_Apoio_Data]    Script Date: 4/8/2025 3:47:34 PM ******/
+DROP TABLE [dbo].[dtx_tb_Rel_Database_Apoio_Data]
+GO
+USE [DBADataex]
+GO
+/****** Object:  Table [dbo].[dtx_tb_Rel_Database_ApoioFinal]    Script Date: 4/8/2025 3:47:34 PM ******/
+DROP TABLE [dbo].[dtx_tb_Rel_Database_ApoioFinal]
+GO
+USE [DBADataex]
+GO
+/****** Object:  Table [dbo].[dtx_tb_Rel_Database_ApoioRelaPT2]    Script Date: 4/8/2025 3:47:34 PM ******/
+DROP TABLE [dbo].[dtx_tb_Rel_Database_ApoioRelaPT2]
+GO
+USE [DBADataex]
+GO
+/****** Object:  Table [dbo].[dtx_tb_Rel_Database_ApoioWaits]    Script Date: 4/8/2025 3:47:34 PM ******/
+DROP TABLE [dbo].[dtx_tb_Rel_Database_ApoioWaits]
+GO
+USE [DBADataex]
+GO
+/****** Object:  Table [dbo].[dtx_tb_Rel_Disco_Apoio]    Script Date: 4/8/2025 3:47:34 PM ******/
+DROP TABLE [dbo].[dtx_tb_Rel_Disco_Apoio]
+GO
+USE [DBADataex]
+GO
+/****** Object:  Table [dbo].[dtx_tb_Rel_Disco_Apoio_Data]    Script Date: 4/8/2025 3:47:34 PM ******/
+DROP TABLE [dbo].[dtx_tb_Rel_Disco_Apoio_Data]
+GO
+USE [DBADataex]
+GO
+/****** Object:  Table [dbo].[dtx_tb_Rel_Disco_ApoioFinal]    Script Date: 4/8/2025 3:47:34 PM ******/
+DROP TABLE [dbo].[dtx_tb_Rel_Disco_ApoioFinal]
+GO
+USE [DBADataex]
+GO
+/****** Object:  Table [dbo].[dtx_tb_Rel_Disco_ApoioRelaPT2]    Script Date: 4/8/2025 3:47:34 PM ******/
+DROP TABLE [dbo].[dtx_tb_Rel_Disco_ApoioRelaPT2]
+GO
