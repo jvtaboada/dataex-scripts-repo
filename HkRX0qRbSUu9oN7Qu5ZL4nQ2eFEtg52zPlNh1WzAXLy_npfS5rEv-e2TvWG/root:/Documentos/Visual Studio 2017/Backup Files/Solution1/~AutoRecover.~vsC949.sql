76b45c5b4166610a64aa4f992a4350f7f4b46296
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-04-16 16:29:12 -0300

    Upload de arquivo automático do Power Automate

---
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsC949.sql
@@ -0,0 +1,19 @@
+SELECT TOP (10) [id]
+      ,[subject]
+      ,[body]
+      ,[sender]
+      ,[email_date]
+      ,[instance]
+      ,[email_date1]
+      ,[email_date2]
+  FROM [dbo].[dtx_tb_alerts_emails]
+
+
+ BEGIN TRAN 
+
+ UPDATE [dbo].[dtx_tb_alerts_emails]
+ SET sender = 'EMAILERRADO@fudeu.com.br'
+ --WHERE id = 1;
+
+ COMMIT;
+ ROLLBACK
