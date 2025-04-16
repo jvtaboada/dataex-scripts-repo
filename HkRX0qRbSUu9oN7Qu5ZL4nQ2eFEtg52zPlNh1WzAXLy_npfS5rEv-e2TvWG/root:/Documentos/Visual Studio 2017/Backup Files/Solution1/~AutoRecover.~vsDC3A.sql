5c58a577b0ac3802d83392f7a2d30d229d4d5f70
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-04-16 16:46:02 -0300

    Upload de arquivo automático do Power Automate

--- HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsDC3A.sql
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsDC3A.sql
@@ -1,6 +1,23 @@
-SELECT *
-INTO TBSGO_ReclassificacaoMensalBacen_BKP20250416_GMUD4830
-FROM TBSGO_ReclassificacaoMensalBacen
-
+--SELECT *
+--INTO TBSGO_ReclassificacaoMensalBacen_BKP20250416_GMUD4830
+--FROM TBSGO_ReclassificacaoMensalBacen;
 
 -- nomenclatura GMUD: BKP20250416_GMUD4830
+
+
+
+begin tran
+
+
+
+-- QDT de linhas alteradas = 1
+Update bdfgcsgo..TBSGO_ReclassificacaoMensalBacen
+set CD_ReclassificacaoMensalBacenStatus = '3'
+where ID_ReclassificacaoMensalBacen = '108'
+
+ 
+
+--Antes de efetuar o commit verificar se a quantidade de linhas alteradas é correponte ao resultado
+--Caso não efetuar rolback e devolver o chamado com o print do resultado.
+--commit
+--rolback
