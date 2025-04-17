4d6ad3ad2d34c810604597ffb3230cbe4ce3d18b
João Vitor <95289947+jvtaboada@users.noreply.github.com>
2025-04-17 17:00:34 -0300

    Upload de arquivo automático do Power Automate

---
+++ HkRX0qRbSUu9oN7Qu5ZL4nQ2eFEtg52zPlNh1WzAXLy_npfS5rEv-e2TvWG/root:/Documentos/Visual Studio 2017/Backup Files/Solution1/~AutoRecover.~vsFEED.sql
@@ -0,0 +1,12 @@
+SELECT 
+    SERVERPROPERTY('MachineName') AS NomeServidor,
+    SERVERPROPERTY('ServerName') AS NomeInstancia,
+    SERVERPROPERTY('Edition') AS Edicao,
+    SERVERPROPERTY('ProductVersion') AS Versao,
+    SERVERPROPERTY('EngineEdition') AS TipoInstancia,
+    SERVERPROPERTY('IsClustered') AS Clusterizado,
+    SERVERPROPERTY('InstanceDefaultDataPath') AS CaminhoDados,
+    SERVERPROPERTY('InstanceDefaultLogPath') AS CaminhoLogs;
+
+
+SELECT @@VERSION;
